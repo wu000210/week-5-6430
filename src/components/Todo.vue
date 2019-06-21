b1dada53ac3beccabd3cd8d295a4d5c70faff1a2
riqin wu <wu000210@algonquinlive.com>
2019-06-21 12:15:49 -0400

    Added a form and slot in the form compnent

---
+++ src/components/Todo.vue
@@ -0,0 +1,26 @@
+<template>
+    <ul>
+        <li v-for="(todo, index) in todos" :key="index"
+        @click="todoDone(index)">
+         {{index}} - {{ todo }}
+        </li>
+    </ul>
+</template>
+<script>
+export default {
+    prop: {
+      todos: {
+        type: Array,
+        required: true
+        }
+    },
+methods: {
+    todoDone(indes) {
+        console.log(index);
+        //$emit emots and event from the child component to the parent component
+        this.emit("removeTodo", index);
+    }
+ }
+};
+</script>
+
---
+++ src/views/TodoList.vue
@@ -0,0 +1,53 @@
+<template>
+    <div>
+        <h1>Todo List</h1>
+        <TodoForm @newTodo="addTodo" >
+        <h2 slot="title">Add a Todo</h2>
+        <p slot="desc">Your Todos will be saved</p>
+        </TodoForm>
+        <Todo :todos="todoList" @removeTodo="appDeleteTodo"/>
+    </div>
+</template>
+<script>
+import Todo from "@/components/Todo.vue";
+import TodoForm from "@/components/TodoForm.vue";
+
+export default{
+    data() {
+        return {
+            todoList: ["Walk the dog", "Go for a ride"]
+        };
+    },
+    components: {
+        Todo,
+        TodoForm
+    },
+    methods: {
+    appDeleteTodo(index) {
+        this.todoList.splice(index, 1); 
+    },
+    addTodo(todo){
+       this.todoList.push(todo);
+    }
+   }
+};
+</script>
+
+
+
+<style>
+ul {
+    list-style: name;
+    with: 50%;
+    margin: 0, auto;
+    }
+    ul li{
+        border-bottom: 1px solid;
+        padding: 10px 0;
+        margin-bottom: 10px;
+
+    }
+
+
+</style>
+
